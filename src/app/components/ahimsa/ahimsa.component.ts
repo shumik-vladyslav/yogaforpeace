ad66277a362f1cc626a712de789f98a0dee23c8a
joy-tee <brahmajoytee@gmail.com>
2022-08-21 22:42:04 +0300

    cfg

--- src/app/components/ahimsa/ahimsa.component.ts
+++ src/app/components/ahimsa/ahimsa.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { emailValidator, phoneValidator } from 'src/app/services/validation.service';
 
 @Component({
   selector: 'app-ahimsa',
@@ -6,10 +10,136 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ahimsa.component.scss']
 })
 export class AhimsaComponent implements OnInit {
+  form: FormGroup;
+  url = "https://b24-ay5iam.bitrix24.eu/rest/4/95igs0uaxwczeh83/";
+  routeParams;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private http: HttpClient,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {
+    this.form = new FormGroup({
+      name: new FormControl('', Validators.required),
+      email: new FormControl('', [emailValidator()]),
+      phone: new FormControl('', [Validators.required, phoneValidator()]),
+    });
   }
 
+  ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe((params) => {
+      console.log(params);
+      this.routeParams = params;
+      // const userId = params['userId'];
+      // console.log(userId);
+    });
+  }
+
+  goToURl(url) {
+    window.open(url);
+  }
+
+  sendRegistrationData() {
+    return;
+    const form = this.form.value;
+    console.log('sendRegistrationData');
+    this.http
+      .post(
+        this.url + 'crm.deal.list',
+        {}
+      )
+      .subscribe((dealListResponse: any) => {
+        let dealListTotal = dealListResponse.total;
+        this.http
+          .post(
+            this.url + 'crm.contact.list',
+            {
+              filter: { "EMAIL": form.email },
+              select: ["ID", "NAME", "LAST_NAME"]
+            },
+          )
+          .subscribe((contactListResponse: any) => {
+            console.log('contactListResponse ', contactListResponse);
+            if (contactListResponse.result.length) {
+              this.useContact(contactListResponse.result[0], dealListTotal);
+            } else {
+              this.addContact(dealListTotal);
+            }
+          });
+      });
+  }
+
+  useContact(contact, dealListTotal) {
+    this.addDeal(contact.ID, dealListTotal);
+  }
+
+  addContact(dealListTotal) {
+    const form = this.form.value;
+    this.http
+      .post(
+        this.url + 'crm.contact.add',
+        {
+          fields: {
+            "NAME": form.name,
+            PHONE: [{ VALUE: form.phone, VALUE_TYPE: "WORK" }],
+            EMAIL: [{ VALUE: form.email, VALUE_TYPE: "HOME" }],
+            UTM_CAMPAIGN: this.routeParams?.utm_campaign,
+            UTM_MEDIUM: this.routeParams?.utm_medium,
+            UTM_SOURCE: this.routeParams?.utm_source,
+            UTM_TERM: this.routeParams?.utm_term,
+          },
+        }
+      )
+      .subscribe((contact: any) => {
+        console.log(contact);
+        this.addDeal(contact.result, dealListTotal);
+      });
+  }
+
+  addDeal(contactId, dealListTotal) {
+    this.http
+      .post(
+        this.url + 'crm.deal.list',
+        {
+          filter: { "CONTACT_ID": contactId, "SOURCE_DESCRIPTION": "Форум Ахимса и Гармония" },
+          select: ["ID", "TITLE", "SOURCE"]
+        },
+      )
+      .subscribe((dealListResponse: any) => {
+        console.log('dealListResponse ', dealListResponse);
+        if (dealListResponse.result.length) {
+          alert("Вы уже зарегестрировались")
+        } else {
+          this.http
+            .post(
+              this.url + 'crm.deal.add',
+              {
+                fields: {
+                  TITLE: `Заявка номер ${dealListTotal + 1}`,
+                  CONTACT_ID: contactId,
+                  STATUS: 'NEW',
+                  OPENED: 'Y',
+                  HAS_PHONE: 'Y',
+                  HAS_EMAIL: 'Y',
+                  STATUS_ID: 'NEW',
+                  STATUS_DESCRIPTION: 'Новый',
+                  SOURCE_ID: 'CALL',
+                  SOURCE_DESCRIPTION: "Форум Ахимса и Гармония",
+                  SOURCE: "Форум Ахимса и Гармония",
+                  UTM_CAMPAIGN: this.routeParams?.utm_campaign,
+                  UTM_MEDIUM: this.routeParams?.utm_medium,
+                  UTM_SOURCE: this.routeParams?.utm_source,
+                  UTM_TERM: this.routeParams?.utm_term,
+
+                },
+              }
+            )
+            .subscribe((res) => {
+              console.log(res);
+              // this.router.navigate['/','thanks'];
+              this.router.navigateByUrl('/thanks');
+            });
+        }
+      });
+  }
 }
