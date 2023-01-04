a6c2b9b1a0ba6690075056c7ccc43d737c30553e
shumik-vladyslav <shumik.vladyslav@gmail.com>
2023-01-04 14:18:45 +0200

    changes

--- src/app/components/forum/thankyou-page/thankyou-page.component.ts
+++ src/app/components/forum/thankyou-page/thankyou-page.component.ts
@@ -10,10 +10,28 @@ export class ThankyouPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.load()
   }
 
 
   goToLink(url: string){
     window.open(url, "_blank");
   }
+
+
+  load() {
+    (function (f: any, b, e, v, n, t, s) {
+        if (f.fbq) return; n = f.fbq = function () {
+            n.callMethod ?
+                n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+        }; if (!f._fbq) f._fbq = n;
+        n.push = n; n.loaded = !0; n.version = '2.0'; n.queue = []; t = b.createElement(e); t.async = !0;
+        t.src = v; s = b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t, s)
+    })(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+    (window as any).fbq.disablePushState = true; //not recommended, but can be done
+    (window as any).fbq('init', '626453055020322');
+    (window as any).fbq('track', 'PageView');
+    console.log('Facebook pixel init run!')
+}
+    // (window as any).fbq('track', 'PageView');
 }
