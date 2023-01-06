d501fee478266c873dc3d2c4b8e12adf5fa74e0d
shumik-vladyslav <shumik.vladyslav@gmail.com>
2023-01-06 10:36:52 +0200

    fix

--- src/app/components/forum/thankyou-page/thankyou-page.component.ts
+++ src/app/components/forum/thankyou-page/thankyou-page.component.ts
@@ -21,7 +21,7 @@ export class ThankyouPageComponent implements OnInit {
     this.lan = language;
   }
   ngOnInit(): void {
-    this.load()
+    this.addScriptsToHead()
   }
 
   goToLink(url: string){
@@ -64,18 +64,49 @@ export class ThankyouPageComponent implements OnInit {
   //     _fbq.push(['track', 'PageView']);
   // })();
 
-  (function (f: any, b, e, v, n, t, s) {
-    if (f.fbq) return; n = f.fbq = function () {
-        n.callMethod ?
-            n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-    }; if (!f._fbq) f._fbq = n;
-    n.push = n; n.loaded = !0; n.version = '2.0'; n.queue = []; t = b.createElement(e); t.async = !0;
-    t.src = v; s = b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t, s)
-})(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
-(window as any).fbq.disablePushState = true; //not recommended, but can be done
-(window as any).fbq('init', '626453055020322');
-(window as any).fbq('track', 'PageView');
+//   (function (f: any, b, e, v, n, t, s) {
+//     if (f.fbq) return; n = f.fbq = function () {
+//         n.callMethod ?
+//             n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+//     }; if (!f._fbq) f._fbq = n;
+//     n.push = n; n.loaded = !0; n.version = '2.0'; n.queue = []; t = b.createElement(e); t.async = !0;
+//     t.src = v; s = b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t, s)
+// })(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+// (window as any).fbq.disablePushState = true; //not recommended, but can be done
+// (window as any).fbq('init', '626453055020322');
+// (window as any).fbq('track', 'PageView');
     console.log('Facebook pixel init run!')
+}
+
+addScriptsToHead() {
+
+
+  const head1 = document.getElementsByTagName('head')[0];
+
+
+  const script1 = document.createElement('noscript');
+  script1.innerHTML = `<img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=626453055020322&ev=PageView&noscript=1"
+/>`;
+
+  head1.insertBefore(script1, head1.firstChild);
+
+  const head = document.getElementsByTagName('head')[0];
+
+
+  const script = document.createElement('script');
+  script.innerHTML = `  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '626453055020322');
+  fbq('track', 'Lead');`;
+
+  head.insertBefore(script, head.firstChild);
 }
     // (window as any).fbq('track', 'PageView');
 }
