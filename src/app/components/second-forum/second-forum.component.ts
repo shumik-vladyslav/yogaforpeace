e276bac4c4c734efa6fd4489dbb4eded8bc5d546
shumik-vladyslav <shumik.vladyslav@gmail.com>
2023-01-07 12:32:52 +0200

    fix

--- src/app/components/second-forum/second-forum.component.ts
+++ src/app/components/second-forum/second-forum.component.ts
@@ -312,4 +312,67 @@ export class SecondForumComponent implements OnInit {
 
     head.insertBefore(script, head.firstChild);
   }
+
+  addScriptsToscript() {
+
+
+    const head1 = document.getElementsByTagName('body')[0];
+
+
+    const script1 = document.createElement('script');
+    script1.innerHTML = `    const links = document.querySelectorAll('a');
+
+    if( links ) {
+        for( const link of links ) {
+            if (
+                link.href.includes("tg://resolve") ||
+                link.href.includes("https://t.me/") ||
+                link.href.includes("https://direct.smartsender.com/redirect") ||
+                link.href.includes("viber://pa") ||
+                link.href.includes("https://vk.com/app") ||
+                link.href.includes("vk://vk.com/app") ||
+                link.href.includes("https://m.me") ||
+                link.href.includes("https://wa.me") ||
+                link.href.includes("whatsapp://send")
+            ) {
+                link.classList.add('ss-btn');
+            }
+        }
+    }
+`;
+
+    head1.insertBefore(script1, head1.firstChild);
+
+    const head = document.getElementsByTagName('body')[0];
+
+
+    const script = document.createElement('script');
+    script.innerHTML = `     const getUrlParam = function( param ) {
+      if ( ! param ) return;
+
+      const queryString = window.location.search;
+      const urlParams = new URLSearchParams( queryString );
+      const paramValue = urlParams.get(param);
+
+      // console.log(paramValue);
+      return paramValue;
+    };
+
+
+    // ssDeepLink params
+    const ssContext = {
+        variables: {
+            utm_source: getUrlParam('utm_source'), // получит параметр "utm_source" из ссылки
+            utm_medium: getUrlParam('utm_medium'), // получит параметр "utm_medium" из ссылки
+            utm_campaign: getUrlParam('utm_campaign'), // получит параметр "utm_campaign" из ссылки
+            utm_content: getUrlParam('utm_content'), // получит параметр "utm_content" из ссылки
+            W_chanel: getUrlParam('W_chanel'), // получит параметр "W_chanel" из ссылки
+        }
+    };
+    console.log(ssContext);
+    ssDeepLink('ss-btn', 'yogiespeas', false, ssContext);
+`;
+
+    head.insertBefore(script, head.firstChild);
+  }
 }
