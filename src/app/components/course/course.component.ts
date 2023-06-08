43df6602af59b9f1c7269217d4830cb54f3aa699
joy-tee <brahmajoytee@gmail.com>
2023-06-08 14:26:07 +0300

    slider

--- src/app/components/course/course.component.ts
+++ src/app/components/course/course.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { tns } from "../../../../node_modules/tiny-slider/src/tiny-slider"
 
 @Component({
   selector: 'app-course',
@@ -10,6 +11,16 @@ export class CourseComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    tns({
+      "container": '.my-slider',
+      "items": 5,
+      "arrowKeys": true,
+      "swipeAngle": false,
+      "speed": 400
+    });
   }
 
 }
