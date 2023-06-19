c0c299547fa7be754abb316ea8f056b6b3128d98
joy-tee <brahmajoytee@gmail.com>
2023-06-19 15:30:31 +0300

    ff

--- src/app/components/course/course.component.ts
+++ src/app/components/course/course.component.ts
@@ -14,6 +14,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class CourseComponent implements OnInit {
   hasExpand = {};
+  menu: boolean = false;
   form: FormGroup;
   url = "https://vosd.bitrix24.eu/rest/4/idujiuxkvouf7pb9/";
   isSendRegistrationMessage: boolean = false;
@@ -230,4 +231,7 @@ export class CourseComponent implements OnInit {
       });
   }
 
+  showMenu() {
+    this.menu = true;
+  }
 }
